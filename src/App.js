ccd8b9c6380c5b168aeaaf459174da500e5080bc
Sean Tan <seantanyurong@gmail.com>
2024-10-21 11:29:29 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Reset from "./Reset";
-import Dashboard from "./Dashboard";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Reset from './Reset';
+import Dashboard from './Dashboard';
 
+// Test
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/reset' element={<Reset />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
